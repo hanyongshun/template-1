ff2622e7d9bdab169a57cd36cf4fe5e6958adb93
sollecitom <michele.sollecito@gmail.com>
2013-09-15 17:18:09 +0200

    Initial version

---
+++ template-parent/template-application/src/main/java/it/ms/template/application/App.java
@@ -0,0 +1,13 @@
+package it.ms.template.application;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
